2035af3a059bbc041d420495c0b9027fc837458e
TarangChavhan <chavhannaman2@gmail.com>
2025-09-29 23:14:31 +0530

    Array Indexing

---
+++ 02 Array Indexing/oneD.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,5,6])
+print("Printing single Element :-",arr[2])
+print("Adding two Array Element :- ", arr[1]+arr[2])
+print("Adding Array Element with 2 :-",arr[3+2])
